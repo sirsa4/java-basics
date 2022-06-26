442cef40cf0cd6d9519f6f03073becfe687876aa
sirsa4 <yaya.abbdi@gmail.com>
2022-06-26 22:22:44 +0200

    class basic setup

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import classBasic.Student;
+import dataTypes.DataTypes;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+      /*  DataTypes data = new DataTypes();
+        DataTypes.types();*/
+
+        Student student1 = new Student();
+
+        student1.firstName = "Mario";
+        student1.lastName = "Mario";
+        student1.age = 22;
+        student1.studentID = "1325";
+
+        student1.studentInformation();
+
+        Student student2 = new Student("Luigi","Mario",23,"3258");
+        student2.studentInformation();
     }
 }
